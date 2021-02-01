487ce5e0cf71e3dc8753f74623a6a93360ea1c51
gdlkk <mateusz.d.mateusz@gmail.com>
2021-02-01 23:08:29 +0100

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by Mateusz Dettlaff on 01/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
